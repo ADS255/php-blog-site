6a2f178091d16e817843f7b4f1f1a6b2ed0393ab
Abhay Sharma <86826529+ADS255@users.noreply.github.com>
2024-05-26 12:16:57 +0100

    post url added to displayed posts.

--- app/Views/home.php
+++ app/Views/home.php
@@ -3,8 +3,11 @@
         <?php if (!empty($posts) && is_array($posts)): ?>
             <?php foreach ($posts as $post): ?>
                 <li>
-                    <h2><?= esc($post['post_title']); ?></h2>
-                    <p><?= esc($post['post_content']); ?></p>
+                    <h2>
+                        <a href="<?= base_url('blogpost/' . esc($post['id'])); ?>">
+                            <?= esc($post['post_title']); ?>
+                        </a>
+                    </h2>
                 </li>
             <?php endforeach; ?>
         <?php else: ?>
